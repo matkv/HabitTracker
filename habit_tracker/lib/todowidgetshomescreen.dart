f686380c1ed57c8d137c957ed23e1a21466ce648
Matko <matkobusljeta@gmail.com>
2019-12-23 19:39:15 +0100

    Started working on pop-up dialog for todo widgets on homescreen

--- habit_tracker/lib/todowidgetshomescreen.dart
+++ habit_tracker/lib/todowidgetshomescreen.dart
@@ -59,81 +59,176 @@ class _ToDoWidgetsHomeScreenState extends State<ToDoWidgetsHomeScreen> {
 
   List<Widget> createToDoPreviews(AsyncSnapshot snapshot) {
     return snapshot.data
-        .map<Widget>((habit) => Card(
-                child: Container(margin: EdgeInsets.only(top: 5.0),
+        .map<Widget>( //TODO move this to own widget class
+          (habit) => Card(
+            child: Container(
+              margin: EdgeInsets.only(top: 5.0),
               height: 200,
               width: 250,
-              child: Flex(
-                direction: Axis.horizontal,
-                children: <Widget>[
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () async {
+                  bool shouldUpdate = await showDialog(
+                    context: context,
+                    child: Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                      child: Container(
+                        width: 100,
+                        height: 300,
+                        margin: EdgeInsets.all(5),
+                        child: Flex(
+                          direction: Axis.vertical,
                           children: <Widget>[
-                            Icon(
-                              habit.icon,
-                              color: Colors.red,
-                              size: 20.0,
+                            Expanded(
+                              flex: 1,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        habit.title,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                            Text(
-                              habit.title,
-                              style: TextStyle(fontSize: 17),
+                            Expanded(
+                              flex: 3,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Flexible(
+                                    child: Text(
+                                      habit.description,
+                                      style: TextStyle(fontSize: 15),
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
+                            Divider(),
+                            Expanded(
+                              flex: 1,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.date_range,
+                                    color: Colors.red,
+                                  ),
+                                  Text(
+                                    DateFormat.yMMMMd("en_US").format(habit.duedate),
+                                    style: TextStyle(fontSize: 15, ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.check),
+                                    onPressed: () => Navigator.pop(context, true),
+                                  ),
+                                )),
                           ],
                         ),
-                        Divider(
-                          indent: 10.0,
-                          endIndent: 10.0,
-                          thickness: 1.0,
-                          color: Colors.red,
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Flexible(
-                                child: Container(
-                                  margin: EdgeInsets.all(10.0),
-                                  child: Text(
-                                    habit.description,
-                                    style: TextStyle(fontSize: 15),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Divider(),
-                        Expanded(
-                          flex: 1,
-                          child: Row(
+                      ),
+                    ),
+                  );
+
+                  setState(() {
+                    //TODO react to what should happen once task is marked as done
+                    // send update command to database that updates the "done" value (TODO)
+                    //shouldUpdate ? reload data somehow
+                  });
+                },
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
                               Icon(
-                                Icons.date_range,
+                                habit.icon,
                                 color: Colors.red,
+                                size: 20.0,
                               ),
                               Text(
-                                DateFormat.yMMMMd("en_US")
-                                    .format(habit.duedate),
-                                style: TextStyle(
-                                  fontSize: 15,
-                                ),
-                              )
+                                habit.title,
+                                style: TextStyle(fontSize: 17),
+                              ),
                             ],
                           ),
-                        ),
-                      ],
+                          Divider(
+                            indent: 10.0,
+                            endIndent: 10.0,
+                            thickness: 1.0,
+                            color: Colors.red,
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(
+                                  child: Container(
+                                    margin: EdgeInsets.all(10.0),
+                                    child: Text(
+                                      habit.description,
+                                      style: TextStyle(fontSize: 15),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.date_range,
+                                  color: Colors.red,
+                                ),
+                                Text(
+                                  DateFormat.yMMMMd("en_US")
+                                      .format(habit.duedate),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            )))
+            ),
+          ),
+        )
         .toList();
   }
 }
