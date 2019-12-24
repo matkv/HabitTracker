ee2aa9b0d3fc607a345a914ee72a36a0c08dcf6e
Matko <matkobusljeta@gmail.com>
2019-12-24 01:08:08 +0100

    Move pop up dialog to separate class and file

--- habit_tracker/lib/todowidgetshomescreen.dart
+++ habit_tracker/lib/todowidgetshomescreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:habit_tracker/habit.dart';
 import 'package:habit_tracker/habitdatabase.dart';
+import 'package:habit_tracker/popupdetails.dart';
 import 'package:intl/intl.dart';
 
 class ToDoWidgetsHomeScreen extends StatefulWidget {
@@ -70,83 +71,7 @@ class _ToDoWidgetsHomeScreenState extends State<ToDoWidgetsHomeScreen> {
                 onTap: () async {
                   bool shouldUpdate = await showDialog(
                     context: context,
-                    child: Dialog(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16),
-                        ),
-                      ),
-                      child: Container(
-                        width: 100,
-                        height: 300,
-                        margin: EdgeInsets.all(5),
-                        child: Flex(
-                          direction: Axis.vertical,
-                          children: <Widget>[
-                            Expanded(
-                              flex: 1,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Column(
-                                    children: <Widget>[
-                                      Text(
-                                        habit.title,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Flexible(
-                                    child: Text(
-                                      habit.description,
-                                      style: TextStyle(fontSize: 15),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Divider(),
-                            Expanded(
-                              flex: 1,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.date_range,
-                                    color: Colors.red,
-                                  ),
-                                  Text(
-                                    DateFormat.yMMMMd("en_US").format(habit.duedate),
-                                    style: TextStyle(fontSize: 15, ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                                flex: 1,
-                                child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: FloatingActionButton(
-                                    child: Icon(Icons.check),
-                                    onPressed: () => Navigator.pop(context, true),
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                    ),
+                    child: PopUpDetails(context: context, habit: habit,),
                   );
 
                   setState(() {
