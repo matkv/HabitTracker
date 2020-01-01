f9fb9be13f5685fdd302765cdd49c17e3188d642
Matko <matkobusljeta@gmail.com>
2020-01-01 13:05:59 +0100

    Different todo widget on homescreen - first draft

--- habit_tracker/lib/todowidgetshomescreen.dart
+++ habit_tracker/lib/todowidgetshomescreen.dart
@@ -75,115 +75,115 @@ class _ToDoWidgetsHomeScreenState extends State<ToDoWidgetsHomeScreen> {
 
   List<Widget> createToDoPreviews(AsyncSnapshot snapshot) {
     return snapshot.data
-        .map<Widget>(
-          //TODO move this to own widget class
-          (habit) => Card(
-            child: Container(
-              margin: EdgeInsets.only(top: 5.0),
-              height: 200,
-              width: 250,
-              child: GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                onTap: () async {
-                  bool shouldUpdate = await showDialog(
-                    context: context,
-                    child: PopUpDetails(
-                      context: context,
-                      habit: habit,
-                    ),
-                  );
-
-                  setState(() {
-                    //TODO react to what should happen once task is marked as done
-                    // send update command to database that updates the "done" value (TODO)
-                    //shouldUpdate ? reload data somehow
-                  });
-                },
+        .map<Widget>((habit) => Card(
                 child: Flex(
-                  direction: Axis.horizontal,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Icon(
-                                habit.icon,
-                                color: Colors.red,
-                                size: 20.0,
-                              ),
-                              Text(
-                                habit.title,
-                                style: TextStyle(fontSize: 17),
-                              ),
-                            ],
+              direction: Axis.horizontal,
+              children: <Widget>[
+                Expanded(
+                  flex: 0,
+                  child: Container(
+                    height: 200,
+                    width: 250,
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () async {
+                        bool shouldUpdate = await showDialog(
+                          context: context,
+                          child: PopUpDetails(
+                            context: context,
+                            habit: habit,
                           ),
-                          Divider(
-                            indent: 10.0,
-                            endIndent: 10.0,
-                            thickness: 1.0,
-                            color: Colors.red,
+                        );
+
+                        setState(() {
+                          //TODO react to what should happen once task is marked as done
+                          // send update command to database that updates the "done" value (TODO)
+                          //shouldUpdate ? reload data somehow
+                        });
+                      },
+                      child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  habit.icon,
+                                  size: 35,
+                                  color: Colors.red,
+                                )
+                              ],
+                            ),
                           ),
                           Expanded(
                             flex: 3,
-                            child: Row(
+                            child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Flexible(
-                                  child: Container(
-                                    margin: EdgeInsets.all(10.0),
-                                    child: Text(
-                                      habit.description,
-                                      style: TextStyle(fontSize: 15),
-                                    ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        habit.title,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Flexible(
+                                        child: Text(
+                                          habit.description,
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Divider(),
+                                Expanded(
+                                  flex: 2,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.date_range,
+                                        color: Colors.red,
+                                      ),
+                                      Text(
+                                        DateFormat.yMMMMd("en_US")
+                                            .format(habit.duedate),
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                          Expanded(
-                            flex: 1,
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Icon(
-                                habit.isdone ? Icons.done : Icons.clear,
-                                color: habit.isdone ? Colors.green : Colors.red,
-                                size: 35,
-                              ),
-                            ),
-                          ),
-                          Divider(),
-                          Expanded(
-                            flex: 1,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.date_range,
-                                  color: Colors.red,
-                                ),
-                                Text(
-                                  DateFormat.yMMMMd("en_US")
-                                      .format(habit.duedate),
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                          
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        )
+                  ),
+                )
+              ],
+            )))
         .toList();
   }
 }
