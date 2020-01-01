1211ee4e1a22f062da97f4e6e9474e9f30aa7498
Matko <matkobusljeta@gmail.com>
2020-01-01 21:14:16 +0100

    Changes to daily widget layout

--- habit_tracker/lib/dailywidget.dart
+++ habit_tracker/lib/dailywidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:habit_tracker/habit.dart';
+import 'package:habit_tracker/popupdetails.dart';
 
 class DailyWidget extends StatefulWidget {
   Habit habit;
@@ -20,16 +21,107 @@ class _DailyWidgetState extends State<DailyWidget> {
   Widget build(BuildContext context) {
     return Card(
       child: Container(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[Text(habit.title)],
-            ),
-            Row(
-              children: <Widget>[CircleAvatar(child: Text(habit.activedays[0] == true ? 'Y' : 'N'),)],
-            ),
-             
-          ],
+        height: 200,
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () async {
+            bool shouldUpdate = await showDialog(
+              context: context,
+              child: PopUpDetails(
+                context: context,
+                habit: habit,
+              ),
+            );
+          },
+          child: Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  habit.icon,
+                                  size: 35,
+                                  color: Colors.red,
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 2,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        habit.title,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Flexible(
+                                        child: Text(
+                                          habit.description,
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Divider(),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: habit.activedays
+                                  .map<Widget>((day) => CircleAvatar(
+                                        child: Text(day.toString()),
+                                        backgroundColor:
+                                            day ? Colors.green : Colors.red,
+                                      ))
+                                  .toList()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
