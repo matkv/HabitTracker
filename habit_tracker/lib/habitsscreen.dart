dd64ab1c98a76d1c8fe76b9f5788c97c0e6959ce
Matko <matkobusljeta@gmail.com>
2020-01-09 11:28:05 +0100

    Show list of habit widgets on habitscreen

--- habit_tracker/lib/habitsscreen.dart
+++ habit_tracker/lib/habitsscreen.dart
@@ -1,12 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/habit.dart';
+import 'package:habit_tracker/habitdatabase.dart';
+import 'package:habit_tracker/habitwidget.dart';
 
-class HabitsScreen extends StatelessWidget {
+class HabitsScreen extends StatefulWidget {
+  @override
+  _HabitsScreenState createState() => _HabitsScreenState();
+}
+
+class _HabitsScreenState extends State<HabitsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Habit Screen'),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: HabitWidgetsList(),
+        ),
       ),
     );
+  }
+}
+
+class HabitWidgetsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HabitWidgetListState();
+  }
+}
+
+class _HabitWidgetListState extends State<HabitWidgetsList> {
+  Future<List<Habit>> _future;
+
+  Future<List<Habit>> getHabitsFromDatabase() async {
+    var dbHelper = HabitDatabase.instance;
+    var habits;
+
+    await dbHelper.getHabits().then((value) {
+      //don't call this if the widget has been disposed already
+      if (this.mounted) {
+        setState(() {
+          habits = value;
+        });
+      }
+    });
+
+    return habits;
+  }
+
+  @override
+  void initState() {
+    _future = getHabitsFromDatabase();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        //future: getHabitsFromDatabase(),
+        future: _future,
+        builder: (BuildContext context, AsyncSnapshot<List<Habit>> snapshot) {
+          var widgetToShow;
+
+          if (snapshot.hasData) {
+            if (snapshot.data.length > 0) {
+              var todowidgets = createHabitWidgets(snapshot);
+              widgetToShow = ListView(
+                scrollDirection: Axis.vertical,
+                children: todowidgets,
+              );
+            } else {
+              //show placeholder text if no to-do tasks were created yet
+              widgetToShow = Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'No habits found!',
+                  )
+                ],
+              );
+            }
+          } else {
+            //Progress inditcator while todo tasks are loaded
+            widgetToShow = Center(
+              child: SizedBox(
+                child: CircularProgressIndicator(),
+                height: 20,
+                width: 20,
+              ),
+            );
+          }
+
+          return widgetToShow;
+        });
+  }
+
+  createHabitWidgets(AsyncSnapshot<List<Habit>> snapshot) {
+    return snapshot.data.map<Widget>((habit) => HabitWidget(habit)).toList();
   }
 }
---
+++ habit_tracker/lib/habitwidget.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/habit.dart';
+import 'package:habit_tracker/popupdetails.dart';
+
+class HabitWidget extends StatefulWidget {
+  Habit habit;
+
+  HabitWidget(this.habit);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HabitWidgetState(habit);
+  }
+}
+
+class _HabitWidgetState extends State<HabitWidget> {
+  Habit habit;
+
+  Color checkmarkColor = Colors.grey;
+
+  _HabitWidgetState(this.habit);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        height: 200,
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () async {
+            bool shouldUpdate = await showDialog(
+              context: context,
+              child: PopUpDetails(
+                context: context,
+                habit: habit,
+              ),
+            );
+
+            setState(() {
+              //TODO react to what should happen once task is marked as done
+              // send update command to database that updates the "done" value (TODO)
+              //shouldUpdate ? reload data somehow
+            });
+          },
+          child: Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      habit.icon,
+                      size: 35,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+              ),
+
+              Expanded(
+                flex: 3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            habit.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Flexible(
+                            child: Text(
+                              habit.description,
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.whatshot,
+                            color: Colors.orange,
+                          ),
+                          Text(
+                            'TODO Streak',
+                            style: TextStyle(fontSize: 15, ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Ink(
+                      decoration: ShapeDecoration(
+                        color: checkmarkColor,
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.check),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() {
+                            if (checkmarkColor == Colors.green) {
+                              checkmarkColor = Colors.grey;
+                            } else {
+                              checkmarkColor = Colors.green;
+                            }
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
