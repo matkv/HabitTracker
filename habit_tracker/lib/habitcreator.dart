bf45ba254f23029457ba6be19f4a697a6e081486
Matko <matkobusljeta@gmail.com>
2020-01-11 16:44:13 +0100

    Add error checks to habitcreator

--- habit_tracker/lib/habitcreator.dart
+++ habit_tracker/lib/habitcreator.dart
@@ -7,53 +7,72 @@ import 'package:habit_tracker/habitdatabase.dart';
 class HabitCreator {
   final dbHelper = HabitDatabase.instance;
 
-  void createHabit(String title, String description, String type, IconData icon, DateTime lastupdate, int streakinterval, int streak) {
+  void createHabit(String title, String description, String type, IconData icon,
+      DateTime lastupdate, int streakinterval, int streak) async {
+    Habit currentHabit = new Habit.createHabit(
+        title, description, type, icon, lastupdate, streakinterval, streak);
 
-    Habit currentHabit = new Habit.createHabit(title, description, type, icon, lastupdate, streakinterval, streak);    
-    dbHelper.insertHabit(currentHabit);
+    var successful = await dbHelper.insertHabit(currentHabit);
 
-    Fluttertoast.showToast(msg: "Habit created succesfully"); //TODO implement actual try-catch
+    if (successful == true) {
+      Fluttertoast.showToast(msg: "Habit created succesfully");
+    } else {
+      Fluttertoast.showToast(msg: "Failed to create habit");
+    }
   }
 
-  Future<void> createDaily (String title, String description, String type, IconData icon, List<bool> activedays) async {
-    Habit currentHabit = new Habit.createDaily(title, description, type, icon, activedays);
-    dbHelper.insertHabit(currentHabit);
+  Future<void> createDaily(String title, String description, String type,
+      IconData icon, List<bool> activedays) async {
+    Habit currentHabit =
+        new Habit.createDaily(title, description, type, icon, activedays);
 
-    //get id that has been created when inserting into database
-    int id = await dbHelper.getIdOfNewestDaily();
+    var successful = await dbHelper.insertHabit(currentHabit);
 
-    dbHelper.insertWeekdays(id, currentHabit.activedays);
+    if (successful == true) {
+      //get id that has been created when inserting into database
+      int id = await dbHelper.getIdOfNewestDaily();
+       dbHelper.insertWeekdays(id, currentHabit.activedays);
+
+      
+        Fluttertoast.showToast(msg: "Habit created succesfully");
+      
+    } else {
+      Fluttertoast.showToast(msg: "Failed to create habit");
+    }
+  }
+
+  Future<void> createToDo(String title, String description, String type,
+      IconData icon, DateTime duedate, bool isDone) async {
+    Habit currentHabit =
+        new Habit.createToDo(title, description, type, icon, duedate, isDone);
     
+    bool successfull = await dbHelper.insertHabit(currentHabit);
 
-    Fluttertoast.showToast(msg: "Habit created succesfully");
-  }
-
-  Future<void> createToDo(String title, String description, String type, IconData icon, DateTime duedate, bool isDone){
-    Habit currentHabit = new Habit.createToDo(title, description, type, icon, duedate, isDone);
-    dbHelper.insertHabit(currentHabit);
-
-    Fluttertoast.showToast(msg: "Habit created succesfully");
-
+    if (successfull == true) {
+      Fluttertoast.showToast(msg: "Habit created succesfully");
+    }
+    else{
+      Fluttertoast.showToast(msg: "Failed to create habit");
+    }    
   }
 
   Future<bool> deleteHabit(Habit habit) async {
-    
-  int numberofaffectedrows = await dbHelper.delete(habit.id);
+    int numberofaffectedrows = await dbHelper.delete(habit.id);
 
     if (numberofaffectedrows > 0) {
       return true;
-    }    
-    return false;    
+    }
+    return false;
   }
 
   Future<bool> updateHabit(Habit habit) async {
-
     bool successful = await dbHelper.updateHabit(habit);
 
     if (habit.type == 'daily') {
-      int numberofaffectedrows = await dbHelper.updateWeekdays(habit.id, habit.activedays);
-     
-      if(numberofaffectedrows > 0){
+      int numberofaffectedrows =
+          await dbHelper.updateWeekdays(habit.id, habit.activedays);
+
+      if (numberofaffectedrows > 0) {
         successful = true;
       }
     }
